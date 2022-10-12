6ca69635c63465b7cb33b040053525454b318724
araujo2012 <araujo2012@hotmail.com>
2022-10-12 20:02:35 -0300

    refact: user midlewares  -> added regex verification for email in checkEmail  -> added length verification for password in checkPassword

--- app/backend/src/middlewares/userMidleware.ts
+++ app/backend/src/middlewares/userMidleware.ts
@@ -2,13 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 
 const checkEmail = (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
-  if (!email) return res.status(400).json({ message: 'missing email' });
+  const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  if (!email) return res.status(400).json({ message: 'All fields must be filled' });
+  if (!emailRegex.test(email)) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
   next();
 };
 
 const checkPassword = (req: Request, res: Response, next: NextFunction) => {
   const { password } = req.body;
-  if (!password) return res.status(400).json({ message: 'missing password' });
+  if (!password) return res.status(400).json({ message: 'All fields must be filled' });
+  if (password.length <= 6) return res.status(401).json({ message: 'Incorrect email or password' });
   next();
 };
 
