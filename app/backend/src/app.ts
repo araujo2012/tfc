e49c55efb7316a9c69344ee8c15fcb7f4eea607e
araujo2012 <araujo2012@hotmail.com>
2022-10-12 20:00:32 -0300

    feat: route /login

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import userRoute from './routes/userRoute';
 
 class App {
   public app: express.Express;
@@ -10,6 +11,7 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use('/login', userRoute);
   }
 
   private config():void {
