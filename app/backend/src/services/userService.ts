56b56bb5b8f51a442497f74294e259de1b96daad
araujo2012 <araujo2012@hotmail.com>
2022-10-12 20:04:02 -0300

    feat: userService method getRole

--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -1,13 +1,18 @@
 import userModel from '../database/models/User';
+import decodeToken from '../helpers/authorization';
 
 class userService {
   constructor(private model: typeof userModel) {}
 
   async login(email: string) {
-    const user = await this.model.findOne(
-      { where: { email } },
-    );
+    const user = await this.model.findOne({ where: { email } });
     return { token: user };
+  }
+
+  async getRole(token: string) {
+    const user = decodeToken(token);
+    const checkUser = await this.model.findOne({ where: { email: user.email } });
+    return { role: user.role, validToken: (user === checkUser) };
   }
 }
 
