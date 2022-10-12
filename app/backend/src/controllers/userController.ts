fd6913a445dea3dc6f01fca65220e670c5472685
araujo2012 <araujo2012@hotmail.com>
2022-10-12 20:00:52 -0300

    feat: userController getRole method

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -5,8 +5,19 @@ class userController {
   constructor(private service: userService) {}
 
   async login(req: Request, res: Response) {
-    const result = await this.service.login(req.body);
+    const { email } = req.body;
+    const result = await this.service.login(email);
     return res.status(200).json(result);
+  }
+
+  async getRole(req: Request, res: Response) {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(400).json('missing authorization');
+    const result = await this.service.getRole(authorization);
+    if (result.validToken) {
+      return res.status(200).json({ role: result.role });
+    }
+    return res.status(400).json('Invalid authorization');
   }
 }
 
