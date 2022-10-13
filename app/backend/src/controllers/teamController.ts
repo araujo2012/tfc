acac390c0e3ddc9e5bc52a0debf331c264c60d10
araujo2012 <araujo2012@hotmail.com>
2022-10-13 15:57:23 -0300

    feat: teamController method getTeamById

--- app/backend/src/controllers/teamController.ts
+++ app/backend/src/controllers/teamController.ts
@@ -4,8 +4,14 @@ import teamService from '../services/teamService';
 class teamController {
   constructor(private service: teamService) {}
 
-  async getAll(_req: Request, res: Response) {
+  async getAllTeams(_req: Request, res: Response) {
     const result = await this.service.getAllTeams();
+    return res.status(200).json(result);
+  }
+
+  async getTeamById(req: Request, res: Response) {
+    const { id } = req.params;
+    const result = await this.service.getTeamByid(parseInt(id, 10));
     return res.status(200).json(result);
   }
 }
