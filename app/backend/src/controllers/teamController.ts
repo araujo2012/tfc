410a6e7c9df4202aa924193b95821246a997924d
araujo2012 <araujo2012@hotmail.com>
2022-10-16 19:15:45 -0300

    fix: teamController  -> added try catch inside teamController methods  -> fixed the caall emthod to teamController on teamRoute

--- app/backend/src/controllers/teamController.ts
+++ app/backend/src/controllers/teamController.ts
@@ -1,18 +1,26 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import teamService from '../services/teamService';
 
 class teamController {
   constructor(private service: teamService) {}
 
-  async getAllTeams(_req: Request, res: Response) {
-    const result = await this.service.getAllTeams();
-    return res.status(200).json(result);
+  async getAllTeams(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.service.getAllTeams();
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async getTeamById(req: Request, res: Response) {
-    const { id } = req.params;
-    const result = await this.service.getTeamByid(parseInt(id, 10));
-    return res.status(200).json(result);
+  async getTeamById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const result = await this.service.getTeamByid(parseInt(id, 10));
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- app/backend/src/routes/teamRoute.ts
+++ app/backend/src/routes/teamRoute.ts
@@ -7,7 +7,7 @@ const teamRoute = Router();
 
 const teamController = new TeamController(new TeamService(teamModel));
 
-teamRoute.get('/:id', teamController.getAllTeams);
-teamRoute.get('/', teamController.getTeamById);
+teamRoute.get('/:id', (req, res, next) => teamController.getTeamById(req, res, next));
+teamRoute.get('/', (req, res, next) => teamController.getAllTeams(req, res, next));
 
 export default teamRoute;
