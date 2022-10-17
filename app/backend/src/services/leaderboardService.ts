16eaf0a6eeaaf5ccd7da6d201c2aeaaf45eee715
araujo2012 <araujo2012@hotmail.com>
2022-10-17 16:54:21 -0300

    feat: leaderboardService  -> createAwayLeaderboard  -> createHomeLeaderboard  -> addLeaderboards  -> leaderboard

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -2,6 +2,7 @@ import teamModel from '../database/models/Team';
 import matchModel from '../database/models/Match';
 import ILeaderboard from '../interfaces/ILeaderboard';
 import { IMatch } from '../interfaces/match';
+import ITeam from '../interfaces/ITeam';
 
 const startResult = () => {
   const result = {
@@ -51,6 +52,44 @@ const resolveAwayMatches = (emptyResult: ILeaderboard, teamMatches: Array<IMatch
   return result;
 };
 
+const createAwayLeaderboard = (teams: Array<ITeam>, matches: Array<IMatch>) => teams.map((team) => {
+  const teamMatches = matches
+    .filter((match) => match.awayTeam === team.id)
+    .filter((match) => match.inProgress === 0);
+  const result = startResult();
+  result.name = team.teamName;
+  return resolveAwayMatches(result, teamMatches);
+});
+
+const createHomeLeaderboard = (teams: Array<ITeam>, matches: Array<IMatch>) => teams.map((team) => {
+  const teamMatches = matches
+    .filter((match) => match.homeTeam === team.id)
+    .filter((match) => match.inProgress === 0);
+  const result = startResult();
+  result.name = team.teamName;
+  return resolveHomeMatches(result, teamMatches);
+});
+
+const addLeaderboards = (away: Array<ILeaderboard>, home: Array<ILeaderboard>) => {
+  const leaderboard = away.map((_awayTeam, index) => {
+    const result = {
+      name: away[index].name,
+      totalPoints: away[index].totalPoints + home[index].totalPoints,
+      totalGames: away[index].totalGames + home[index].totalGames,
+      totalVictories: away[index].totalVictories + home[index].totalVictories,
+      totalDraws: away[index].totalDraws + home[index].totalDraws,
+      totalLosses: away[index].totalLosses + home[index].totalLosses,
+      goalsFavor: away[index].goalsFavor + home[index].goalsFavor,
+      goalsOwn: away[index].goalsOwn + home[index].goalsOwn,
+      goalsBalance: away[index].goalsBalance + home[index].goalsBalance,
+      efficiency: String((100 * (away[index].totalPoints + home[index].totalPoints))
+       / (3 * (away[index].totalGames + home[index].totalGames))),
+    };
+    return result;
+  });
+  return leaderboard;
+};
+
 const sortLeaderboard = (leaderboard: Array<ILeaderboard>) => leaderboard.sort((a, b) => {
   if (a.totalPoints !== b.totalPoints) return b.totalPoints - a.totalPoints;
   if (a.goalsBalance !== b.goalsBalance) return b.goalsBalance - a.goalsBalance;
@@ -64,29 +103,24 @@ class userService {
   async leaderboardHome() {
     const matches = await this.MatchModel.findAll();
     const teams = await this.TeamModel.findAll();
-    const homeLeaderboard = teams.map((team) => {
-      const teamMatches = matches
-        .filter((match) => match.homeTeam === team.id)
-        .filter((match) => match.inProgress === 0);
-      const result = startResult();
-      result.name = team.teamName;
-      return resolveHomeMatches(result, teamMatches);
-    });
+    const homeLeaderboard = createHomeLeaderboard(teams, matches);
     return sortLeaderboard(homeLeaderboard);
   }
 
   async leaderboardAway() {
     const matches = await this.MatchModel.findAll();
     const teams = await this.TeamModel.findAll();
-    const awayLeaderboard = teams.map((team) => {
-      const teamMatches = matches
-        .filter((match) => match.awayTeam === team.id)
-        .filter((match) => match.inProgress === 0);
-      const result = startResult();
-      result.name = team.teamName;
-      return resolveAwayMatches(result, teamMatches);
-    });
+    const awayLeaderboard = createAwayLeaderboard(teams, matches);
     return sortLeaderboard(awayLeaderboard);
+  }
+
+  async leaderboard() {
+    const matches = await this.MatchModel.findAll();
+    const teams = await this.TeamModel.findAll();
+    const awayLeaderboard = createAwayLeaderboard(teams, matches);
+    const homeLeaderboard = createHomeLeaderboard(teams, matches);
+    const leaderboard = addLeaderboards(awayLeaderboard, homeLeaderboard);
+    return sortLeaderboard(leaderboard);
   }
 }
 
