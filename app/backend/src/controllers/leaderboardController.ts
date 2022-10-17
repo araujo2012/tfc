18ecca8bb308efec90f9618b9882dd2feef8aad6
araujo2012 <araujo2012@hotmail.com>
2022-10-17 16:03:39 -0300

    feat: leaderboardController  -> leaderboardAway  -> leaderboardhome

---
+++ app/backend/src/controllers/leaderboardController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import leaderboardService from '../services/leaderboardService';
+
+class leaderboardController {
+  constructor(private service: leaderboardService) {}
+
+  async leaderboardHome(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.service.leaderboardHome();
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async leaderboardAway(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.service.leaderboardAway();
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default leaderboardController;
