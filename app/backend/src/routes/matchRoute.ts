19ecb2fcdbec7ce82855bb10189917fc00d3ee9a
araujo2012 <araujo2012@hotmail.com>
2022-10-17 12:51:43 -0300

    feat: matchRoute  -> .post('/')  -> .patch('/:id/finish')  -> .patch('/:id')

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -2,11 +2,28 @@ import { Router } from 'express';
 import MatchController from '../controllers/matchController';
 import MatchService from '../services/matchService';
 import MatchModel from '../database/models/Match';
+import isValidToken from '../middlewares/tokenValidation';
+import { isTeamsTheSame, teamExist } from '../middlewares/matchMidleware';
 
 const matchRoute = Router();
 
 const matchController = new MatchController(new MatchService(MatchModel));
 
 matchRoute.get('/', (req, res, next) => matchController.getAllMatches(req, res, next));
+matchRoute.post(
+  '/',
+  isValidToken,
+  isTeamsTheSame,
+  teamExist,
+  (req, res, next) => matchController.createMatch(req, res, next),
+);
+matchRoute.patch(
+  '/:id/finish',
+  (req, res, next) => matchController.matchHaveFinished(req, res, next),
+);
+matchRoute.patch(
+  '/:id',
+  (req, res, next) => matchController.updateMatchById(req, res, next),
+);
 
 export default matchRoute;
